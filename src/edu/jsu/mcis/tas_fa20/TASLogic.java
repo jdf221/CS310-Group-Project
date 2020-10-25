a0b085b2962c0d68d04ff7ab487843a414ca3bdb
Hunzman81 <48723717+Hunzman81@users.noreply.github.com>
2020-10-25 16:53:35 -0500

    Finished  calculateTotalMinutes function

--- src/edu/jsu/mcis/tas_fa20/TASLogic.java
+++ src/edu/jsu/mcis/tas_fa20/TASLogic.java
@@ -1,5 +1,30 @@
 package edu.jsu.mcis.tas_fa20;
 
-public class TASLogic {
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
+public class TASLogic {
+    public static int calculateTotalMinutes(ArrayList<Punch> punchList, Shift shift) {
+        int totalMinutes = 0;
+        int punchCount = 0;
+        GregorianCalendar lastInstant = null;
+
+        for (Punch punch : punchList) {
+            if (punch.type == Punch.PunchType.ClockIn) {
+                punchCount++;
+                lastInstant = punch.getAdjustedInstant();
+            } else if (punch.type == Punch.PunchType.ClockOut) {
+                punchCount++;
+                if (lastInstant != null) {
+                    totalMinutes += TimeUnit.MINUTES.convert(Math.abs(punch.getAdjustedInstant().getTimeInMillis() - lastInstant.getTimeInMillis()), TimeUnit.MILLISECONDS);
+                }
+            }
+        }
+
+        if (punchCount == 2 && totalMinutes >= shift.shiftDuration) {
+            totalMinutes -= shift.lunchDuration;
+        }
+
+        return totalMinutes;
+    }
 }
