1150b7a1e857bd472232c9b6b2e8e643de744223
AStrach <70605813+AStrach@users.noreply.github.com>
2020-10-25 17:04:49 -0500

    Rewrote Shift.java
    better variable names and uses LocalTime

--- src/edu/jsu/mcis/tas_fa20/Shift.java
+++ src/edu/jsu/mcis/tas_fa20/Shift.java
@@ -1,54 +1,47 @@
 package edu.jsu.mcis.tas_fa20;
 
-public class Shift
-{
-    private int ID;
-    private String Name;
-    private String Start;
-    private String End;
-    private int I;
-    private int Grace;
-    private int D;
-    private String LStart;
-    private String LEnd;
-    private int LD;
-    private int Time;
-    private int STime;
-    private int ETime;
-    private int LSTime;
-    private int LETime;
+import java.time.LocalTime;
+import java.time.Duration;
 
-    public Shift(int id, String name, String startTime, String endTime, int interval, int gracePeriod, int dock, String lunchStartTime, String lunchEndTime, int lunchDeduct)
-    {
+public class Shift {
+    public final int id;
+    public final String name;
+    public final LocalTime startTime;
+    public final LocalTime endTime;
+    public final int interval;
+    public final int gracePeriod;
+    public final int dock;
+    public final LocalTime lunchStartTime;
+    public final LocalTime lunchEndTime;
+    public final int lunchDeduct;
+
+    public final long shiftDuration;
+    public final long lunchDuration;
+
+    public Shift(int id, String name, String startTime, String endTime, int interval, int gracePeriod, int dock, String lunchStartTime, String lunchEndTime, int lunchDeduct) {
         // Check pages 6 and 7 of TeamProjectOverview.pdf to learn what all of the variables mean
-        ID = id;
-        Name = name;
-        Start = startTime.substring(0, 5);
-        End = endTime.substring(0, 5);
-        I = interval;
-        Grace = gracePeriod;
-        D = dock;
-        LStart = lunchStartTime.substring(0, 5);
-        LEnd = lunchEndTime.substring(0, 5);
-        LD = lunchDeduct;
-        Time = 24;
-        STime = 720;
-        ETime = 1230;
-        LSTime = 990;
-        LETime = 1020;
+        this.id = id;
+        this.name = name;
+        this.interval = interval;
+        this.gracePeriod = gracePeriod;
+        this.dock = dock;
+        this.lunchDeduct = lunchDeduct;
 
 
+        this.startTime = LocalTime.parse(startTime);
+        this.endTime = LocalTime.parse(endTime);
+        this.lunchStartTime = LocalTime.parse(lunchStartTime);
+        this.lunchEndTime = LocalTime.parse(lunchEndTime);
 
+        this.shiftDuration = Duration.between(this.startTime, this.endTime).toMinutes();
+        this.lunchDuration = Duration.between(this.lunchStartTime, this.lunchEndTime).toMinutes();
     }
 
 
     @Override
-    public String toString()
-    {
+    public String toString() {
         // You need to return a string of the following format: "Shift Name: startTime - endTime (x minutes); Lunch: start - end (x minutes)"
         // Example: "Shift 2: 12:00 - 20:30 (510 minutes); Lunch: 16:30 - 17:00 (30 minutes)"
-
-        return Name + ":" + " " + Start + " " + "-" + " " + End + " " + "(" + (ETime - STime) + " " + "minutes" + ")" + ";" + " " + "Lunch" + ":" + " " + LStart +
-                " " + "-" + " " + LEnd + " " + "(" + (LETime - LSTime) + " " + "minutes" + ")";
+        return name + ": " + startTime + " - " + endTime + " (" + this.shiftDuration + " minutes); Lunch: " + lunchStartTime + " - " + lunchEndTime + " (" + this.lunchDuration + " minutes)";
     }
 }
